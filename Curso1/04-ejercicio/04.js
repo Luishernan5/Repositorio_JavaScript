31d26b5989e74d631577adebcee559c00d9e445e
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-20 05:58:35 -0600

    add

--- Curso1/04-ejercicio/04.js
+++ Curso1/04-ejercicio/04.js
@@ -5,9 +5,9 @@
 let i = 0;
 while (i <= 10){
     if (i % 2 === 0){
-        console.log('Es numero par: ', i);
+        console.log('Es numero par : ', i);
     } else {
-        console.log('Es numero impar: ', i);
+        console.log('Es numero impar : ', i);
     }
     i++;
 }
